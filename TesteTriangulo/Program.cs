a089252603d2ba2de513adf484d5fcfae29f93ae
JESSICA BATISTA DOS SANTOS <jessica21943@hotmail.com>
2025-01-20 19:40:59 -0300

    Exercicio de teste do triangulo

---
+++ TesteTriangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TesteTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ladoA, ladoB, ladoC;
+
+            Console.Write("Digite o valor do lado A: ");
+            ladoA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do lado B: ");
+            ladoB = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do lado C: ");
+            ladoC = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
