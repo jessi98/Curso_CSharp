920b736eea043278638a252f09bb0f1678f48af7
JESSICA BATISTA DOS SANTOS <jessica21943@hotmail.com>
2025-01-20 19:05:07 -0300

    novos exercicios

---
+++ Ex008/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ex008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double velocidade, valor_multa;
+            string multa;
+
+
+            Console.WriteLine("Digite a velocidade do carro");
+            velocidade = Convert.ToDouble(Console.ReadLine());
+
+            valor_multa = (velocidade - 80) * 7;
+
+            if (velocidade > 80)
+            {
+                Console.WriteLine("Você foi multado! O valor a ser pago é: R${0}", valor_multa);
+            }
+            else
+            {
+                Console.WriteLine("Automovel não ultrapassou o limite!");
+            }
+
+
+        }
+    }
+}
---
+++ Ex009/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double numero;
+
+            Console.WriteLine("Digite o número: ");
+            numero = Convert.ToDouble(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("O núemro é par!");
+            }
+            else 
+            {
+                Console.WriteLine("O número é ímpar!");
+            }
+        }
+    }
+}
