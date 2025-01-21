726da1e5714d3428d1f6a1cabfa550ef1911a140
JESSICA BATISTA DOS SANTOS <jessica21943@hotmail.com>
2025-01-20 22:09:49 -0300

    exercicios novos

---
+++ Ex010/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double salario;
+
+            Console.Write("Digite o valor do seu salário: ");
+            salario = Convert.ToDouble(Console.ReadLine());
+
+
+            if (salario > 2500)
+            {
+                Console.WriteLine("O seu salário atual é de: {0}. E o aumento do seu salário é de 5%, o valor total é: {1} ", salario, ((salario * 0.05) + salario));
+            }
+            else if (salario > 1250)
+            {
+                Console.WriteLine("O seu salário atual é de: {0}. E o aumento do seu salário é de 10%, o valor total é: {1}", salario, ((salario * 0.1) + salario));
+            }
+            else 
+            {
+                Console.WriteLine("O seu salário atual é de: {0}. E o aumento do seu salário é de 15%, o valor total é: {1}", salario, ((salario * 0.15) + salario));
+            }
+
+        }
+    }
+}
---
+++ Ex011/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                double distancia_km, preco1, preco2;
+                Console.WriteLine("Digite os KM: ");
+                distancia_km = Convert.ToDouble(Console.ReadLine());
+
+                preco1 = 0.50;
+                preco2 = 0.45;
+                if (distancia_km <= 200)
+                {
+                    Console.WriteLine("A sua distancia foi: {0}km. O valor da passagem é: R${1}", distancia_km, preco1 * distancia_km);
+                }
+                else
+                {
+                    Console.WriteLine("A sua distancia foi: {0}km. O valor da passagem é: R${1}", distancia_km, preco2 * distancia_km);
+                }
+            }
+        }
+    }
+}
