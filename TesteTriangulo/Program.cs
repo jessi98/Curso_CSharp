b84f981942f206d9ad10cd47ad614c7493607e91
JESSICA BATISTA DOS SANTOS <jessica21943@hotmail.com>
2025-01-20 21:08:26 -0300

    projeto completo

--- TesteTriangulo/Program.cs
+++ TesteTriangulo/Program.cs
@@ -18,6 +18,19 @@
             if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoB))
             {
                 Console.WriteLine("Os valores informados formam um triângulo.");
+                if ((ladoA == ladoB) && (ladoB == ladoC))
+                    {
+                    Console.WriteLine("E ele é um triângulo equilátero.");
+                    }
+                else if ((ladoA == ladoB) || (ladoA == ladoC) || (ladoB == ladoC))
+                {
+                    Console.WriteLine("E ele é um triângulo isósceles.");
+                }
+                else
+                {
+                    Console.WriteLine("E ele é um triângulo escaleno.");
+                }
+
             }
             else
             { 
