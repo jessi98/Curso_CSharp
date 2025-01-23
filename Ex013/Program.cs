b015fe78b378d7db83326ccfd5de86b462d18c3d
JESSICA BATISTA DOS SANTOS <jessica21943@hotmail.com>
2025-01-22 21:01:16 -0300

    exercicios 22/01

---
+++ Ex013/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ex013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int final;
+
+            Console.WriteLine("***************************");
+            Console.WriteLine("Todos os pares do intervalo");
+            Console.WriteLine("```````````````````````````");
+            Console.WriteLine();
+
+
+            Console.Write("Digite o final do intervalo: ");
+            final = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= final; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("{0}, ", i);
+                }
+            }
+        }
+    }
+}
