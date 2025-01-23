deda358774c1d02940ecad047c24cac1bf4286b3
JESSICA BATISTA DOS SANTOS <jessica21943@hotmail.com>
2025-01-22 21:58:29 -0300

    exercicio genero

---
+++ Ex016/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex016
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string genero;
+
+            Console.Write("Digite o genero: ");
+            genero = Console.ReadLine().ToUpper();
+
+            while (genero != "F" && genero != "M")
+
+            {
+                Console.Write("Digite o gênero: ");
+                genero = Console.ReadLine();
+            }
+
+            Console.WriteLine("Bem-vindos e bem-vindas ao curso de C#!");
+        }
+    }
+}
