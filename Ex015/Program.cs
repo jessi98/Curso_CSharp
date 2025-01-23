3d8e2abfc83ecb60dbc177c33079ff9c3585d791
JESSICA BATISTA DOS SANTOS <jessica21943@hotmail.com>
2025-01-22 21:39:38 -0300

    exercicio 22/01

---
+++ Ex015/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, mult;
+
+            Console.Write("Digite o número para calcular a tabuada: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i ++) 
+            {
+                mult = num * i;
+                Console.WriteLine("{0} x {1} = {2}", num, i, mult);
+            }
+
+        }
+    }
+}
