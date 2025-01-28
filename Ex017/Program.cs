63c4b80e3c0430654ba6a47eef8618fe8e11b75c
JESSICA BATISTA DOS SANTOS <jessica21943@hotmail.com>
2025-01-27 21:41:23 -0300

    Aula 27/01 - for \ foreach \ do while

---
+++ Ex017/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex017
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 2; // Inicializador de variável
+
+            // Enquanto a condição for verdadeira, o bloco de código será executado
+            /*
+            while (num < 5)
+            {
+                Console.WriteLine("Curso C#");
+            }
+            */
+
+            do
+            {
+                Console.WriteLine("Curso C#");
+                num++;
+            } while (num < 5);
+        }
+    }
+}
---
+++ Teste_Senha/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Teste_Senha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "123";
+            string senhaDigitada;
+            int tentativas = 0;
+
+            do
+            {
+                Console.Clear();
+                Console.Write("Digite a senha: ");
+                senhaDigitada = Console.ReadLine();
+                tentativas++;
+                if (tentativas > 3) { break; }
+            } while (senha != senhaDigitada);
+
+            if (senha != senhaDigitada)
+            {
+                Console.Clear();
+                Console.WriteLine("Senha incorreta! Tentativas: " + tentativas);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Senha correta! Tentativas: " + tentativas);
+            }
+        }
+    }
+}
