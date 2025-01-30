9236667800fa2614cac4f0b652ffafac2ba47ded
JESSICA BATISTA DOS SANTOS <jessica21943@hotmail.com>
2025-01-29 21:27:19 -0300

    Calculando a área do triangulo

---
+++ AreaTriangulo/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AreaTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============================");
+            Console.WriteLine("Calcular a área do triângulo");
+            Console.WriteLine("============================");
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o valor da base do triângulo em metros: ");
+            double ba = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o valor da altura do triângulo em metros: ");
+            double al = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            areaTriangulo(ba, al);
+        }
+
+        static void areaTriangulo (double ba, double al)
+        {
+            double a = (ba * al) / 2;
+            Console.WriteLine($"A área do triângulo de {ba} x {al} é de {a:f2}m²");
+        }
+    }
+}
