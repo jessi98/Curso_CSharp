6384ab55e09330ac0aa914754b656d56406cf372
JESSICA BATISTA DOS SANTOS <jessica21943@hotmail.com>
2025-02-03 21:19:50 -0300

    Fatorial

---
+++ Fatorial/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Fatorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            char resp;
+            bool mostrar = false;
+
+            Console.Write("Digite um número inteiro: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();//pula linha
+
+            Console.WriteLine("Deseja mostrar os cálculos? (s/n)");
+            resp = Convert.ToChar(Console.ReadLine().ToLower());//ToLower se colocar a letra maiuscula
+            Console.WriteLine();
+
+            if (resp == 's')
+            {
+                mostrar = true;
+            }
+            Console.WriteLine(fatorial(num, mostrar));
+        }
+
+        static int fatorial(int n, bool show = false)
+        {
+            int f = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (show)
+                {
+                    Console.Write(i);
+                    if (i < n)
+                    {
+                        Console.Write(" x ");
+                    }
+                    else
+                    {
+                        Console.Write(" = ");
+                    }
+                }
+
+                f *= i;
+
+            }
+            return f;
+        }
+    }
+}
